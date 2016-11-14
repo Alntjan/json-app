85c478bc7d6347b030281b87e6539aa44dcb29e1
Alntjan <oliveiracatarino88@gmail.com>
2016-11-14 17:37:45 +0000

    Testes de Webhook

--- app/jobs/orders_paid_job.rb
+++ app/jobs/orders_paid_job.rb
@@ -4,7 +4,9 @@ class OrdersPaidJob < ActiveJob::Base
 
     shop.with_shopify_session do
       puts "Webhook recebido!"
-
+      # @paid_order = Order.new({ "shop_id": "acme", "shopify_order_id": params[:webhook.], "order_data": "" })
+      puts "Identificação da Loja: #{shop.id}"
+      puts "Identificação da Encomenda: #{webhook.id}"
     end
   end
 end
