66ec5fbb86162af8a660b997bcce2122719e1ce7
Alntjan <oliveiracatarino88@gmail.com>
2016-11-14 22:42:30 +0000

    Webhook

--- app/jobs/orders_paid_job.rb
+++ app/jobs/orders_paid_job.rb
@@ -3,9 +3,12 @@ class OrdersPaidJob < ActiveJob::Base
     shop = Shop.find_by(shopify_domain: shop_domain)
 
     shop.with_shopify_session do
-      puts "Webhook recebido!"
-      @encomenda = Order.new(shop_id: shop.id, shopify_order_id: id, order_data: webhook)
-      @encomenda.save
+      @encomenda = Order.new(shop_id: shop.id, shopify_order_id: webhook[:id], order_data: webhook)
+      if @encomenda.save
+        puts "Webhook recebido e Encomenda feita!"
+      else
+        puts "Erro!"
+      end
     end
   end
 end
