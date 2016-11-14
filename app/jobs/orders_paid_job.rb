9d4e7b7a6d444407917d05cdbdc5cd061c9373b8
Alntjan <oliveiracatarino88@gmail.com>
2016-11-14 18:34:58 +0000

    Webhook

--- app/jobs/orders_paid_job.rb
+++ app/jobs/orders_paid_job.rb
@@ -3,10 +3,7 @@ class OrdersPaidJob < ActiveJob::Base
     shop = Shop.find_by(shopify_domain: shop_domain)
 
     shop.with_shopify_session do
-      puts "Webhook recebido!"
-      data = JSON.parse(request.body.read)
-      puts "Cheguei aqui!"
-      puts "Olha para isto #{data.id}"
+      puts "Webhook recebido! #{shop.id}" 
     end
   end
 end
